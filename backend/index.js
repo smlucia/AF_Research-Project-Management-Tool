f0174cb30115b5849756820cf4941eb99a2f5583
rashmidaswaththa <it20252540@my.sliit.lk>
2022-05-24 20:16:55 +0530

    initialize backend setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express =  require("express");
+const mongoose =  require("mongoose");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.json());
+
+
+mongoose.connect("mongodb+srv://user_1212:user123123@clusterno1.1hpvx.mongodb.net/rpmt_db?retryWrites=true&w=majority");
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB databse successfully connected. ');
+})
+
+//add routes here
+
+app.get("/" , (req, res) => {
+    res.send({Project :" MernCrud"})
+});
+
+const PORT = process.env.PORT || 5005;
+app.listen(PORT);
