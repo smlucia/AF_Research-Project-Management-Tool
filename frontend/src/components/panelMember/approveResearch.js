79467224cf6396335079e95ddea37be51dd38610
Manindri <IT20246778@my.sliit.lk>
2022-06-02 21:30:26 +0530

    implement approve research page

---
+++ frontend/src/components/panelMember/approveResearch.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import '../../styles/admin/AdminNavBar.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+let id = 0;
+function createData(groupId, leaderEmail, researchTopic, academicYear, approved) {
+  id += 1;
+  return { id, groupId, leaderEmail, researchTopic, academicYear, approved };
+}
+
+const rows = [
+  createData('SE3060_WD_01', 'kawshi@gmail.com', "Automobile", 4, "True"),
+  createData('SE3060_WD_01', 'kawshi@gmail.com', "Automobile", 4, "True"),
+  createData('SE3060_WD_01', 'kawshi@gmail.com', "Automobile", 4, "True")
+
+];
+
+
+const ApproveResearch = () => {
+    
+        return (
+            <>
+                <div>
+                    <nav>
+                        <div className="logo">Approve Research Topics</div>
+                    </nav>
+                
+                </div>
+
+            {/* create a table */}
+            <Paper className={styles.root}>
+                <Table className={styles.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">Group ID</TableCell>
+                            <TableCell align="right">Leader Email</TableCell>
+                            <TableCell align="right">Research Topic</TableCell>
+                            <TableCell align="right">Academic Year</TableCell>
+                            <TableCell align="right">Approved</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => {
+                            return (
+                                <TableRow key={row.id}>
+                                    <TableCell align="right">{row.groupId}</TableCell>
+                                    <TableCell align="right">{row.leaderEmail}</TableCell>
+                                    <TableCell align="right">{row.researchTopic}</TableCell>
+                                    <TableCell align="right">{row.academicYear}</TableCell>
+                                    <TableCell align="right">{row.approved}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+            </>
+        );
+}
+
+export default ApproveResearch;
